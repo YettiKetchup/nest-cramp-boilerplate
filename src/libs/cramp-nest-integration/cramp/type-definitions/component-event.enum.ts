3383664c1673bad6fa9e1e5abf9fb8fe4949fbc4
dstolpnyk <d.stolpnyk@twinwingames.com>
2021-02-12 20:10:59 +0200

    Remove submodule

--- src/libs/cramp-nest-integration/cramp/type-definitions/component-event.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum ComponentEvent {
-    ATTACH = 'ComponentEvent:ATTACH',
-    DEATTACH = 'ComponentEvent:DEATTACH',
-    CHANGE = 'ComponentEvent:Change'
-}
--- src/libs/cramp-nest-integration/cramp/utils/sleep.utility.ts
+++
@@ -1,5 +0,0 @@
-export const sleep = (time: number): Promise<any> => {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(null), time);
-    });
-}
